6a6556c1cb66ebae493a99348614e025ea7735dd
jacebryan425 <nieserja@gmail.com>
2026-06-09 19:01:05 -0600

    Added Periwinkle Town buildings

--- include/constants/metatile_labels.h
+++ include/constants/metatile_labels.h
@@ -375,8 +375,8 @@
 #define METATILE_Pacifidlog_SubmergedLogs_VerticalTop          0x25A
 
 // gTileset_Petalburg
-#define METATILE_Petalburg_Door_BirchsLab   0x249
-#define METATILE_Petalburg_Door_Littleroot  0x248
+#define METATILE_Petalburg_Door_BirchsLab   0x22B
+#define METATILE_Petalburg_Door_Littleroot  0x249
 #define METATILE_Petalburg_Door_Oldale      0x287
 
 // gTileset_PetalburgGym
